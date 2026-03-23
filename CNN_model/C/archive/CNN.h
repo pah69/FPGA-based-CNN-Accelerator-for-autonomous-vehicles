16d64e4e6aea2cf712c1fab47d4a6854d0985f31
pah69 <engineerpah@gmail.com>
2026-03-23 13:49:14 +0700

    new

--- CNN_model/C/archive/CNN.h
+++
@@ -1,1 +0,0 @@
-void CNN(float InModel[784],float &OutModel0,float Weights[4996]);
--- CNN_model/C/float32/Dense.h
+++
@@ -1,2 +0,0 @@
-void Dense_0(float input_Dense[250],float output_Dense[16],float bias[16],float weight[4000]);
-void Dense_1(float input_Dense[16],float &output_Dense0,float bias[10],float weight[160]);
