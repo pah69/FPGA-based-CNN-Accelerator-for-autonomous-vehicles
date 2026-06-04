b6972ff73283f1465421fd10c1306b5cf0b40c23
pah69 <engineerpah@gmail.com>
2026-06-04 10:32:31 +0700

    new changes

--- src/synth/tpu_ip_v3/tpu_ip_v3.runs/synth_1/tpu_top_axi_lite.tcl
+++ src/synth/tpu_ip_v3/tpu_ip_v3.runs/synth_1/tpu_top_axi_lite.tcl
@@ -125,6 +125,8 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
 set_param ips.enableIPCacheLiteLoad 1
+
+read_checkpoint -auto_incremental -incremental /home/pah/fpga_cnn_accelerator/FPGA-based-CNN-Accelerator-for-autonomous-vehicles/src/synth/tpu_ip_v3/tpu_ip_v3.srcs/utils_1/imports/synth_1/tpu_top_axi_lite.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
